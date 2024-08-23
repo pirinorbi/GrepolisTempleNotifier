3142e983f6d6f567daf569556e89f37350490df6
joswigchert <contact@joswigchert.nl>
2024-08-23 20:48:43 +0200

    moved baseurl of the script to vercel url

--- public/script.user.js
+++ public/script.user.js
@@ -24,7 +24,7 @@
 var uw = unsafeWindow || window;
 var $ = uw.jQuery;
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://grepolis-temple-notifier.vercel.app";
 
 // ======================================
 const settings = {
