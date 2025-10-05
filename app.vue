e9549bcaba3aa06de1fd3d3ebac7dbd2982dae4b
JosWigchert <joswigchert@gmail.com>
2025-10-05 20:40:53 +0200

    Fix homepage

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div
+    class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 p-6 text-center">
+    <div class="p-8 rounded-2xl shadow-lg bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm">
+      <h1 class="text-3xl font-bold mb-2 text-gray-800 dark:text-white">Grepolis Temple Notifier</h1>
+      <p class="text-gray-600 dark:text-gray-300">
+        by
+        <a href="https://github.com/JosWigchert" target="_blank"
+          class="text-blue-600 dark:text-blue-400 hover:underline font-medium">
+          JosWigchert
+        </a>
+      </p>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({ title: 'Grepolis Temple Notifier' })
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['~/assets/css/main.css'],
   routeRules: {
     '/api/**': {
-        cors: true,
+      cors: true,
     },
   },
 })
